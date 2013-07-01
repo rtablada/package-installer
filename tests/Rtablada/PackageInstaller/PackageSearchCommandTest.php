183731dba7a9913e7421b171b3749ab3e0b77e73
Ryan Tablada <ryan.tablada@gmail.com>
2013-07-01 16:00:24 -0400

    Now we are test driving this

---
+++ tests/Rtablada/PackageInstaller/PackageSearchCommandTest.php
@@ -0,0 +1,9 @@
+<?php namespace Rtablada\PackageInstaller;
+
+class PackageSearchCommandTest extends \PHPUnit_Framework_Testcase
+{
+	public function testSearchesForPackages()
+	{
+		new PackageSearchCommand;
+	}
+}
